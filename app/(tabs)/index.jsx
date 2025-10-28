ba4192d63514a40dbf051843960fb6719f22f87b
Radwan Mansur <raydiaz1899@gmail.com>
2025-10-28 02:26:31 +0200

    image upload

--- app/(tabs)/index.jsx
+++ app/(tabs)/index.jsx
@@ -1,9 +1,18 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ImageBackground } from 'expo-image';
+import { StyleSheet, Text, View } from 'react-native';
 
+import job1 from "@/assets/images/job3.jpg";
+
+console.log('hi')
 const app = () => {
   return (
     <View style={styles.container}>
-      <Text style={styles.text}>Coffe shop</Text>
+      <ImageBackground
+        source={job1}
+        style={styles.image}
+      >
+        <Text style={styles.text}>Coffe shop</Text>
+      </ImageBackground>
     </View>
   )
 }
@@ -20,5 +29,15 @@ const styles = StyleSheet.create({
     color: 'white',
     fontSize: 42,
     textAlign: 'center'
+    , backgroundColor: 'rgba(0,0,0,0.7)'
+  }
+  ,
+  image: {
+    resizeMode: "cover",
+    width: '100%',
+    height: '100%',
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center'
   }
 })
